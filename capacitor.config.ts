fecfd4299322ba8a1c1bbb1fd8c5f3d9ada24f66
alexisgomez17 <alexigomez231@gmail.com>
2025-03-06 20:53:08 +0200

    Prueba 1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MiPrimerApp',
+  webDir: 'www'
+};
+
+export default config;
